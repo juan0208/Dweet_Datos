0918e444fd8e174513a85755d59b43aeff4ee401
JaimeDiaz <jedt1996@gmail.com>
2023-03-25 13:02:01 -0500

    Evio a Dweet

--- src/main.cpp
+++ src/main.cpp
@@ -1,60 +1,150 @@
 #include <Arduino.h>
 #include <WiFi.h>
-//crear constante para un red inalambrica
-const char * nombre = "LAPTOP-HL7K9H6R 2729";
-const char * password = "3|4254Ey";
-const char * host ="www.google.com";
-const int puerto =80;
+#include "DHT.h"
+#include <Adafruit_Sensor.h>
 
+#define DHTPIN 4  //Pin de datos del sensor DHT11/22
+
+#define DHTTYPE DHT11  //Especificamos el tipo de sensor DHT11 DHT22 DHT21
+
+DHT dht(DHTPIN, DHTTYPE);  //Creamos el objeto que representa al sensor
+const char* ssid = "LAPTOP-HL7K9H6R 2729";
+const char* password =  "3|4254Ey";
+const char* host = "dweet.io";
+
+
+String getTipoCifrado(wifi_auth_mode_t tipoCifrado) {
+ 
+  switch (tipoCifrado) {
+    case (WIFI_AUTH_OPEN):
+      return "Open";
+    case (WIFI_AUTH_WEP):
+      return "WEP";
+    case (WIFI_AUTH_WPA_PSK):
+      return "WPA_PSK";
+    case (WIFI_AUTH_WPA2_PSK):
+      return "WPA2_PSK";
+    case (WIFI_AUTH_WPA_WPA2_PSK):
+      return "WPA_WPA2_PSK";
+    case (WIFI_AUTH_WPA2_ENTERPRISE):
+      return "WPA2_ENTERPRISE";
+  }
+}
+ 
+void scanNetworks() {
+ 
+  int numberOfNetworks = WiFi.scanNetworks();
+ 
+  Serial.print("Numero de redes encontradas: ");
+  Serial.println(numberOfNetworks);
+ 
+  for (int i = 0; i < numberOfNetworks; i++) {
+ 
+    Serial.print("Nombre de red: ");
+    Serial.println(WiFi.SSID(i));
+ 
+    Serial.print("Fuerza de la señal: ");
+    Serial.println(WiFi.RSSI(i));
+ 
+    Serial.print("Direccion MAC: ");
+    Serial.println(WiFi.BSSIDstr(i));
+ 
+    Serial.print("Tipo de cifrado: ");
+    String tipoCifradoDescription = getTipoCifrado(WiFi.encryptionType(i));
+    Serial.println(tipoCifradoDescription);
+    Serial.println("-----------------------");
+ 
+  }
+}
+ 
+void connectToNetwork() {
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Estableciendo conexion WiFi..");
+  }
+ 
+  Serial.println("Conectado a la red");
+ 
+}
+ 
 void setup() {
-  // code
-  // put your setup code here, to run once:
-  pinMode(2,OUTPUT); // coloco el pin 2 como salida
-  Serial.begin(115200);//confuguracion de velocidad de wifi
-  Serial.println("Inicializado el dispocitovo");
-  WiFi.begin(nombre,password);
-
-  Serial.printf("Estableciendo vinculo con el Ap.");
-  while (WiFi.status() !=WL_CONNECTED){
-  Serial.print(".");
-  delay(1000);
-    
-  }
-  Serial.println("\r\n Conecion establecida");
-  Serial.println("numero IP asignado");
-  Serial.println(WiFi.localIP());//imprime el numero ip
-  Serial.println();
+ 
+  Serial.begin(115200);
+  dht.begin();
+ 
+  scanNetworks(); //Escanear las redes WiFi disponibles
+  connectToNetwork();  //Conectarse a la red que especificamos arriba
+ 
+  Serial.println(WiFi.macAddress());
+  Serial.println(WiFi.localIP());
+ 
+//  WiFi.disconnect(true);
+//  Serial.println(WiFi.localIP());
+ 
+  Serial.println("Ensayo del DHT22!");
 }
-
+ 
 void loop() {
-  WiFiClient Cliente;
-  //
-  if(!(Cliente.connect(host,puerto))){
-    Serial.println("Erro conexion la host fallida");
-    delay(3000);
-    return;
+   Serial.println("Conectandose a:");
+   Serial.println(host);
+
+   WiFiClient client;
+
+   const int httpPort = 80;
+   
+   if(!client.connect(host, httpPort)){
+     Serial.println("Conexion Fallida");
+     delay(1000);
+     return;
+   }
+
+   delay(2000);
+   float humedad = dht.readHumidity(); //Leemos la humedad en % de humedad relativa (0 al 100%)
+   float temperatura = dht.readTemperature(); //Leemos la temperatura en °C
+   
+   //Verificamos las lecturas del sensor
+   if(isnan(humedad) || isnan(temperatura)){
+     Serial.println("Fallo la lectura del sensor");
+     return;
+   }
+
+   //Calculamos la sensacion termica o indice de calor en celsius
+   float indice = dht.computeHeatIndex(temperatura, humedad, false);
+
+   Serial.print(F("Humedad: ")); //La F() siginifica que lo que esta dentro del parentesis se escribe en la FLASH
+   Serial.print(humedad);
+   Serial.print(F("%  Temperatura: ")); //La F() siginifica que lo que esta dentro del parentesis se escribe en la FLASH
+   Serial.print(temperatura);
+   Serial.print(F("°C  Indice de calor: ")); //La F() siginifica que lo que esta dentro del parentesis se escribe en la FLASH
+   Serial.print(indice);
+
+  //String cadena = "hola"+String("mundo")+String(variable);
+  String url = "/dweet/for/ucevaJaime?temperatura="+String(temperatura)+"&humedad="+String(humedad)+"&indice="+String(indice);
+
+  //Se pueden leer los dweets en  https://dweet.io/get/latest/dweet/for/uceva10
+
+  Serial.print("Solicitando el recurso: ");
+  Serial.println(url);
+
+  client.print(String("GET ") + url + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" 
+                    + "Connection: close\r\n\r\n");
+  unsigned long timeout = millis(); //Leo la hora
+  while(client.available() == 0){   //Mientras hayan cero letras disponibles...
+      if(millis() - timeout > 5000){ //Resto la hora actual a la hora inicial, si es mas de 5s
+        Serial.println("Expiro el tiempo de espera");
+        client.stop();
+        return;
+      }
   }
-  Serial.print("conexion al host exitosa");
+
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.println(line);
+  }
   
-  // peticion GET al servidor http
-  Cliente.print("GET /index.html HTTP/1.1\r\nHost: "+String(host)+"\r\n"+"Connection: close\r\n\r\n");
-
-  //Debemos darle un tiempo al servidor a que responda la peticion
-  //delay(5000); // no funciona aqui bloequea el wifi bloqueante
-  unsigned long milisegundos=millis(); //hora de inicio
-
-while(Cliente.available()==0){// preguntamos si no hay datos recibidos disponibles
-    if(millis()-milisegundos>7000){
-      Serial.println("se expiro el tiempo de la conexion");
-      Cliente.stop();
-    }
-}
-
-while (Cliente.available()){
-  String linea = Cliente.readStringUntil('\r');
-  Serial.println(linea);
-}
-  Serial.println("Fin de la conexion");
-  Cliente.stop();
-  delay(2000);
+  Serial.println("Fin de conexion");
+  client.stop();
+  delay(20000);
 }
